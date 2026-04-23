157717b6af0178e9550732fcae0780a22f097374
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 19:12:59 +0600

    Add Repository

---
+++ Repositories/ILibraryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Repositories;
+
+public interface ILibraryRepository
+{
+    void Add(Book book);
+    bool SaveChanges();
+}
---
+++ Repositories/LibraryRepository.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagementAPI.Data;
+using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Repositories;
+
+public class LibraryRepository:ILibraryRepository
+{
+    private readonly AppDbContext _context;
+    
+    public LibraryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(Book book)
+    {
+        _context.books.Add(book);
+    }
+
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() > 0;
+    }
+}
