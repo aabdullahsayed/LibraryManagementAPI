46ea344d0c3a1f7af8a75e39fd05d398d9d051a9
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 22:10:59 +0600

    Changed SQLite to SQL Server

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ILibraryService, LibraryService>();
 builder.Services.AddScoped<ILibraryRepository, LibraryRepository>();
