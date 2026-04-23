3fb0126adc2be6072a6a691f074827b8cdf7db90
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 21:03:49 +0600

    Register Services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using LibraryManagementAPI.Data;
+using LibraryManagementAPI.Repositories;
+using LibraryManagementAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ILibraryService, LibraryService>();
+builder.Services.AddScoped<ILibraryRepository, LibraryRepository>();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.MapControllers();
 
 
 app.Run();
