92170993e45ad78de7c7672472ffd548f6d9f5e4
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 19:12:29 +0600

    Add DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementAPI.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Book> books { get; set; }
+    public DbSet<Member> members { get; set; }
+    public DbSet<Loan> loans { get; set; }
+    public DbSet<AuditLog> auditlogs { get; set;}
+    
+}
