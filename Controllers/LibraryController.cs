7524c4bbb2305d755e06b98ae4673b628a45c0c9
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 19:11:17 +0600

    Add Library Controller

---
+++ Controllers/LibraryController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementAPI.Models;
+using LibraryManagementAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LibraryController:ControllerBase
+{
+    private readonly ILibraryService _service;
+
+    public LibraryController(ILibraryService service)
+    {
+        _service = service;
+    }
+    
+    [HttpPost]
+    public IActionResult AddBook(Book book)
+    {
+        return Ok(_service.AddBook(book));
+    }
+}
