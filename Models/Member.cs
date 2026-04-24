bb2c87a9ac6b07f8bcae91815c8daf64757e4f05
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-24 08:21:25 +0600

    Make the IsActive Nullable

--- Models/Member.cs
+++ Models/Member.cs
@@ -5,5 +5,5 @@ public class Member
     public int Id { get; set; }
     public string FullName { get; set; }
     public string email { get; set; }
-    public bool IsActive { get; set; }
+    public bool? IsActive { get; set; }
 }
