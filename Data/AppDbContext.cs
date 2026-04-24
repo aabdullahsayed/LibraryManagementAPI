a60b3a5463391dafcfe43d47c7f08c3ebce51c77
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-24 23:05:43 +0600

    Add constrains using FluentApi and add the Interceptor in the DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagementAPI.Models;
+﻿using LibraryManagementAPI.Interceptors;
+using LibraryManagementAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagementAPI.Data;
@@ -14,5 +15,44 @@ public class AppDbContext:DbContext
     public DbSet<Member> members { get; set; }
     public DbSet<Loan> loans { get; set; }
     public DbSet<AuditLog> auditlogs { get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(b => b.Title)
+                    .HasMaxLength(150);
+
+                entity.Property(b => b.ISBN)
+                    .IsRequired().
+                    HasMaxLength(17);
+                entity.HasIndex(b => b.ISBN)
+                    .IsUnique();
+            } 
+        );
+
+        modelBuilder.Entity<Member>(entity =>
+            {
+                entity.Property(m => m.email)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.HasIndex(m => m.email)
+                    .IsUnique();
+
+                entity.Property(m => m.IsActive).HasDefaultValue(true);
+                
+                entity.HasQueryFilter(m => m.IsActive == true);
+            } 
+        );
+    }
     
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(new AuditInterceptor());
+        base.OnConfiguring(optionsBuilder);
+    }
 }
