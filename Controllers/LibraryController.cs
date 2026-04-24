3876abe17c378f41795c69bbb7d99a295ac094ac
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-24 23:06:21 +0600

    UPDATE Action method putting the Task and await

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagementAPI.Models;
+﻿using LibraryManagementAPI.DTOs;
+using LibraryManagementAPI.Models;
 using LibraryManagementAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,9 +15,42 @@ public class LibraryController:ControllerBase
         _service = service;
     }
     
-    [HttpPost]
-    public IActionResult AddBook(Book book)
+    [HttpPost("books")]
+    public async Task<IActionResult> AddBook(Book book)
     {
-        return Ok(_service.AddBook(book));
+        return Ok(await _service.AddBook(book));
     }
+
+    [HttpPost("members")]
+    public async Task <IActionResult> AddMember(Member member)
+    {
+        return Ok(await _service.AddMember(member));
+    }
+    
+    [HttpPost("loans")]
+    public async Task<IActionResult> BorrowBook([FromBody] LoanDto request)
+    {
+        try
+        {
+            var loan = await _service.BorrowBookAsync(request);
+            return Ok(loan);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message });
+        }
+    }
+    
+    [HttpGet("loans")]
+    public async Task<IActionResult> GetLoans()
+    {
+        var loans = await _service.GetAllLoansAsync();
+    
+        return Ok(loans);
+    }
+    
 }
